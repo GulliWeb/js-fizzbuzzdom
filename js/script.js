1b7e1b06bf4289d21827aa16514a1e6dabf8d714
Andrea Gulli <andreagulli11@gmail.com>
2024-08-08 16:11:05 +0200

    Milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // Stampo in console i numeri da 1 a 100, per i multipli di 3 stampo 'Fizz', per quelli di 5 stampo 'Buzz'
-
-for (let i = 0 ; i <= 100; i++) {
-    if (i % 3 == 0) {
-        console.log('Fizz')
+for (let i = 1 ; i <= 100; i++) {
+    let output = '';
+    if (i % 3 == 0 && i % 5 == 0) {
+        output += 'FizzBuzz'
     } else if (i % 5 == 0 ) {
-        console.log('Buzz')
-    } else {
-        console.log(i)
+        output += 'Buzz'
+    } else if (i % 3 == 0) {
+        output += 'Fizz'
+    } else{
+        output = (i)
     }
+    console.log(output);
 }
+  
