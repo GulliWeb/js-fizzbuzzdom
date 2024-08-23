b568cce5b495c6e1f0e5717ff4e4ed9c3defda1e
Andrea Gulli <andreagulli11@gmail.com>
2024-08-23 23:15:43 +0200

    FizzBuzzDom completed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 // Stampo in console i numeri da 1 a 100, per i multipli di 3 stampo 'Fizz', per quelli di 5 stampo 'Buzz'
 for (let i = 1 ; i <= 100; i++) {
-    let output = '';
+     const cell = document.createElement('div')
+     let output = '';
+
     if (i % 3 == 0 && i % 5 == 0) {
         output += 'FizzBuzz'
+        cell.classList.add('cell-fizz-buzz')
     } else if (i % 5 == 0 ) {
         output += 'Buzz'
+        cell.classList.add('cell-buzz')
     } else if (i % 3 == 0) {
         output += 'Fizz'
+        cell.classList.add('cell-fizz')
     } else{
         output = (i)
     }
-    console.log(output);
+
+    // Creo e stampo gli elementi nel DOM
+    const cellContainer = document.getElementById('cell-container')
+    cell.classList.add('cell')
+    cell.innerHTML = output
+    cellContainer.appendChild(cell)
 }
-  
